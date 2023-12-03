8cf02531e8ed93314f46e76dc6ae16f38072a973
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-03 17:52:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,11 @@ public class Main {
 		
 		JFrame frame = new JFrame();
 		
+		frame.setTitle("windowOne");
 		frame.setSize(400,400);
 		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		
 
 	}
