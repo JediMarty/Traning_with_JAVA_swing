a6685a468249a841a03c8434bae1a632f848d8b0
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-03 17:47:12 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package train2;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.setSize(400,400);
+		frame.setVisible(true);
+		
+
+	}
+
+}
