f726135abb7fac1aeb523eabf07a1866ba56619b
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-04 18:11:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package train2;
 
 import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.*;
 
 public class Main {
@@ -14,9 +16,20 @@ public class Main {
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(Color.BLUE);
-        //frame.getContentPane().setBackground(new Color(0x123456));
+		frame.getContentPane().setBackground(new Color(0x123456));
+		
+		//ImageIcon image = new ImageIcon("snow.png");
+		
+		JLabel label = new JLabel();
+		label.setText("Snowy!");
+		label.setForeground(Color.white);
+		label.setFont(new Font("Tahoma",Font.BOLD,20));
+	    //label.setIcon(image);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setVerticalAlignment(JLabel.TOP);
+	    
+		frame.add(label);
 		
 	}
 
-}}
+}
