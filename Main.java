427ac803f2680a8208ab42ed80757f8743aff23c
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-05 19:58:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,18 +8,21 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		ImageIcon image = new ImageIcon("snow.png");
+		Border border = BorderFactory.createLineBorder(Color.BLUE, 6);
 		
 		JLabel label = new JLabel();
 		label.setText("Snowy!");
 		label.setForeground(Color.white);
 		label.setFont(new Font("Tahoma",Font.BOLD,40));
-	    label.setIcon(image);
-	    label.setVerticalTextPosition(JLabel.TOP);
-	    label.setHorizontalTextPosition(JLabel.CENTER);
-	    label.setVerticalAlignment(JLabel.TOP);
-	    label.setHorizontalAlignment(JLabel.CENTER);
-	
+		label.setIcon(image);
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalAlignment(JLabel.TOP);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setBorder(border);
+	    
 	    
 		JFrame frame = new JFrame();
 		
