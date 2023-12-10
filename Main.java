0f17d73e1849e898e472e896ecde3901be447418
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-10 18:10:34 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package train2;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,10 +16,11 @@ public class Main implements ActionListener{
 		ImageIcon image = new ImageIcon("snow.png");
 		Border border = BorderFactory.createLineBorder(Color.BLUE, 6);
 	    JButton button = new JButton();
-		
+		JTextField field = new JTextField();
+	    
 	    JLabel label = new JLabel();
 		label.setText("Snowy!");
-		label.setForeground(Color.white);
+		label.setForeground(Color.black);
 		label.setFont(new Font("Tahoma",Font.BOLD,40));
 	    label.setIcon(image);
 	    label.setVerticalTextPosition(JLabel.TOP);
@@ -26,26 +28,28 @@ public class Main implements ActionListener{
 	    label.setVerticalAlignment(JLabel.TOP);
 	    label.setHorizontalAlignment(JLabel.CENTER);
 	    label.setBorder(border);
-	    label.setVisible(false);
-	   
-	    
+	
 	    button.setBounds(100, 200, 200, 50);
 	    button.setText("Click!");
 	    button.setFocusable(false);
 	    button.setBackground(Color.LIGHT_GRAY);
-	    button.addActionListener(e-> label.setVisible(true));
-		
+	    //button.addActionListener();
+	    
+	
+	    field.setBounds(100, 200, 200, 50);
+	    field.setHorizontalAlignment(JLabel.CENTER);
+		field.setForeground(Color.black);
 	    JFrame frame = new JFrame();
 		
-		frame.setTitle("windowOne");
-		
+		frame.setTitle("Xmas-WishList");
+		frame.setBackground(new Color(0x123456));
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(new Color(0x123456));
-		frame.setSize(400,400);
-		frame.add(button);
 		frame.add(label);
+		frame.pack();	
+		frame.add(button);
+        frame.add(field);
 		
 	
 	}
