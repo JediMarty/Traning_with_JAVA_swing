7ce2344076471574325df7f6ce51cf00183a23ac
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-12-13 13:13:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package train2;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.ImageIcon;
@@ -10,9 +11,9 @@ import javax.swing.JTextField;
 
 
 public class Main extends JFrame{
-	JButton button;
+	
 	Main() {
-		
+		JButton button = new JButton();
 		JLabel label = new JLabel();
 		ImageIcon img = new ImageIcon("snow.png");
 		JTextField field = new JTextField();
@@ -20,23 +21,21 @@ public class Main extends JFrame{
 		label.setIcon(img);
 		label.setBackground(new Color(0x123456));
 		label.setOpaque(true);
+		label.add(field);
+		label.add(button);
 		
-		button = new JButton();
+		field.setBounds(100,100,200,50);
+		
 		button.setText("paste");
 		button.setBounds(100, 200, 200, 40);
 		button.setFocusable(false);
 		
-		field.setBounds(100,100,200,50);
-		
+		this.setTitle("list");
 		this.setSize(400, 400);
 		this.setResizable(false);
-		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.getContentPane().setBackground(Color.cyan);
+		this.add(label);
 		this.setVisible(true);
-		//this.add(label);
-		this.add(button);
-		this.add(field);
 		
 	}
 	
